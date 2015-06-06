78a1be464dc3de22b1cc363ca921823ac69512d3
Koustubh <koustubh89@gmail.com>
2015-06-07 03:12:58 +0530

    adding the root controller

---
+++ app/scripts/controllers/registeredUserCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('newEagleApp')
+.controller('registeredUserController', ["$scope","SuggestionService", "$interval","$location", function ($scope, SuggestionService, $interval, $location) {
+
+	console.log('registered user');
+
+	
+}]);

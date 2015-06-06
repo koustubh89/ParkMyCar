19b4ecea0c952f0497a03e852f9c6c99945fff8a
Koustubh <koustubh89@gmail.com>
2015-06-06 09:43:08 +0530

    intermediate commit uptill free user page

--- app/scripts/controllers/freeUSerCtrl.js
+++ app/scripts/controllers/freeUSerCtrl.js
@@ -3,23 +3,29 @@ angular.module('newEagleApp')
 .controller('freeUserCtrl', ["$scope","SuggestionService", "$interval", function ($scope, SuggestionService, $interval) {
 
 	var generateSuggesstions = function(){
-		SuggestionService.getData('getSuggestions', false, 'get', {'search': $scope.searchFor}).then(function(response){
-			$scope.suggestions = response;
-		});
+		$scope.suggestions = SuggestionService.getLocationLatLong($scope.searchFor);
 	}
 
+	$scope.availabilty 	= '28';
+	$scope.currentSuggestion = '';
+
 	var statusCall = function(){
-		SuggestionService.getData('availabilityAndChart', false ,'get').then(function(response){
+		SuggestionService.getData('availabilityAndChart', false ,'get', {'':$currentSuggestion}).then(function(response){
 			$scope.chartData 	= response.chartData;
 			$scope.availabilty 	= response.availabilty;
 		});
 	};
 
-	$scope.changeSuggestion = function(){
+	$scope.changeSuggestion = function(idx){
+		$currentSuggestion = $scope.suggestions[idx];
 		statusCall();
 	};
 
-	generateSuggesstions();
+	$scope.searchChanged = function(value){
+		generateSuggesstions(value);
+	}
+
+	// generateSuggesstions();
 	// $interval(statusCall, 5000);
 	
 }]);
--- app/scripts/services/suggestions.js
+++ app/scripts/services/suggestions.js
@@ -1,6 +1,46 @@
 'use strict';
 angular.module('newEagleApp')
 .factory("SuggestionService", function($http, $injector, $q) {
+
+	var callback = function (results, status) {
+	  	if (status == google.maps.places.PlacesServiceStatus.OK) {
+	    	//for (var i = 0; i < results.length; i++) {
+	      	//createMarker(results[i]);
+	    	//}
+	    	return results;
+	  	}
+	};
+	var initialize = function(longitude, lattitude) {
+	  	var pyrmont = new google.maps.LatLng(longitude, lattitude);
+
+	  	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+	    	center: pyrmont,
+	    	zoom: 15
+	  	});
+
+	  	var request = {
+	    	location: pyrmont,
+	    	radius: 1000,
+	    	types: ['food']
+	  	};
+	  	// infowindow = new google.maps.InfoWindow();
+	  	var service = new google.maps.places.PlacesService(map);
+		// var result = service.nearbySearch(request, callback);
+		var status =true;
+		var finalResult = [];
+		service.nearbySearch(request, function (results, status) {
+		  	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		    	//for (var i = 0; i < results.length; i++) {
+		      	//createMarker(results[i]);
+		    	//}
+		    	finalResult = results;
+		  	}
+		});
+		// console.log(result);
+		return finalResult;
+	};
+
+
 	return {
 		getData : function(urlMethod, isServer, callType, params) {
 			var q = $injector.get("$q");
@@ -34,6 +74,37 @@ angular.module('newEagleApp')
 				console.error("error block");
 			});		
 			return deferred.promise;		
-		}		
+		},
+		getLocationLatLong : function(address){
+			var geocoder = new google.maps.Geocoder();
+			var lat = '';
+            var lng = '';
+			//--------------------code for calculating the geocodes
+		 	var status = true;
+		  	
+		  	var pyrmont = new google.maps.LatLng(24, 70);
+		 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+		    	center: pyrmont,
+		    	zoom: 15
+		  	});
+
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+	                });
+	                lat = results[0].geometry.location.lat();
+	                lng = results[0].geometry.location.lng();
+	                console.log(lng, lat);
+	                var finalResult = initialize(lng, lat);
+	                return finalResult;
+                } else {
+	                console.log('Geocode was not successful for the following reason: ' + status);
+                    // alert('Geocode was not successful for the following reason: ' + status);
+                }
+        	});
+		}
 	};		
 });
